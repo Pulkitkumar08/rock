fe2ae18f5b09f3bd827badf3ddd8b044d9dc9e78
Pulkitkumar08 <143247049+Pulkitkumar08@users.noreply.github.com>
2023-09-02 01:51:59 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,3 +1,4 @@
+import random
 def get_player_choice():
     while True:
         player_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
@@ -5,3 +6,4 @@ def get_player_choice():
             return player_choice
         else:
             print("Invalid choice. Please enter rock, paper, or scissors.")
+            
