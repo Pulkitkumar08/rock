b11271ed15d335f3831365ea9b33ee4d31ac6574
Pulkitkumar08 <143247049+Pulkitkumar08@users.noreply.github.com>
2023-09-02 02:29:53 +0530

    Update devops.py
    add condition for player vs player

--- devops.py
+++ devops.py
@@ -1,13 +1,20 @@
 import random
 def get_player_choice():
+    def get_player_choice():
+    while True:
+        player_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+        if player_choice in ["rock", "paper", "scissors"]:
+            return player_choice
+        else:
+            print("Invalid choice. Please enter rock, paper, or scissors.");
 def get_computer_choice():
     return random.choice(["rock", "paper", "scissors"])
 def main():
-    print("Welcome to Rock, Paper, Scissors!")
+    print("Welcome to Rock, Paper, "Scissors"!")
     while True:
         mode = input("Choose a mode (computer vs. computer, computer vs. player, player vs. player, or quit): ").lower()
         if mode == "quit":
-            break
+            break;
         elif mode == "computer vs. computer":
             computer1_choice = get_computer_choice()
             computer2_choice = get_computer_choice()
