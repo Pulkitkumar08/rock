134becde26f3a5b9b1706d1aed0122b516a5bdb6
Piyush107193 <143830028+Piyush107193@users.noreply.github.com>
2023-09-01 22:30:02 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,7 @@
+def get_player_choice():
+    while True:
+        player_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+        if player_choice in ["rock", "paper", "scissors"]:
+            return player_choice
+        else:
+            print("Invalid choice. Please enter rock, paper, or scissors.")
